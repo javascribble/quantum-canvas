77fa65e092073a1935ad525037047fa0687c6db8
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-08-04 11:58:15 -0400

    clean up test code

--- test/debug.js
+++ test/debug.js
@@ -1,29 +1,20 @@
 import '/node_modules/@javascribble/quantum/source/main.js';
 import '/source/main.js';
 
-const loadImage = (resource) => {
-    return new Promise((resolve, reject) => {
-        const image = new Image();
-        image.onload = () => resolve(image);
-        image.src = resource;
-    });
-}
-
-loadImage('/test/resources/kal256.png').then(image => {
-    const canvas = document.querySelector('quantum-canvas');
-    canvas.render([
-        {
-            image,
-            sx: 0,
-            sy: 0,
-            sw: 256,
-            sh: 256,
-            dx: 100,
-            dy: 100,
-            dw: 256,
-            dh: 256
-        }
-    ]);
-});
+const canvas = document.querySelector('quantum-canvas');
+const image = document.querySelector('img');
+canvas.drawImages([
+    {
+        image,
+        sx: 0,
+        sy: 0,
+        sw: 256,
+        sh: 256,
+        dx: 100,
+        dy: 100,
+        dw: 256,
+        dh: 256
+    }
+]);
 
 document.body.style.visibility = 'visible';
