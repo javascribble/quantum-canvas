1134fc18611f3f1fe03da9f78a0fad8659032020
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-08-06 09:27:53 -0400

    remove adapter

--- source/adapters/broker.js
+++
@@ -1,8 +0,0 @@
-export const canvasBrokerAdapter = {
-    adapt: (canvas, broker, configuration) => {
-        broker.subscribe('drawSprite', sprite => canvas.drawSprite(sprite));
-    },
-    preserve: (canvas, broker, configuration) => {
-        broker.unsubscribe('drawSprite', sprite => canvas.drawSprite(sprite));
-    }
-};
