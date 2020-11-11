996fee223b32ced63b7bf09db954b4d73e371e7d
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-11-11 10:37:53 -0500

    update adapter format

--- source/adapters/engine.js
+++ source/adapters/engine.js
@@ -1,5 +1,7 @@
 import { Canvas } from '../elements/canvas.js';
 
-Canvas.prototype.adapt = function (api, options) {
-    api.drawSprite = this.drawSprite.bind(this);
+Canvas.prototype.adapt = function (options) {
+    return {
+        drawSprite: this.drawSprite.bind(this)
+    }
 };
