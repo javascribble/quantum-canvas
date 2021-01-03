496a6cdd6245ec6bb60eb32f21f5855147ef4333
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-01-03 10:39:29 -0500

    move tile logic to separate plugin

--- source/adapters/engine.js
+++ source/adapters/engine.js
@@ -1,26 +1,5 @@
 import { Canvas } from '../elements/canvas.js';
 
 Canvas.prototype.adapt = function (api, options) {
-    api.importUniformSheet = (image, sw, sh = sw) => {
-        const sprites = [];
-        for (let row = 0; row < image.height / sh; row++) {
-            for (let column = 0; column < image.width / sw; column++) {
-                sprites.push({ image, sx: column * sh, sy: row * sh, sw, sh });
-            }
-        }
-
-        return sprites;
-    }
-
-    api.calculateTiles = (tiles, divisor) => {
-        for (let index = 0; index < tiles.length; index++) {
-            const tile = tiles[index];
-            tile.dx = tile.sw * (index % divisor);
-            tile.dy = tile.sh * Math.floor(index / divisor);
-            tile.dw = tile.sw;
-            tile.dh = tile.sh;
-        }
-    };
-
     api.drawSprite = this.drawSprite.bind(this);
 };
