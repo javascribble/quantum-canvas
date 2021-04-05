37b61e70a56e04ca7e56fee7904094cdc88eb2f2
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-04-05 09:06:12 -0400

    remove flexible property name temporarily

--- source/extensions/images.js
+++ source/extensions/images.js
@@ -1,24 +1,32 @@
 import { Canvas } from '../elements/canvas.js';
 
+// TODO: Add flexible properties.
+// const defaultProperties = {
+//     children: 'children',
+//     position: 'position',
+//     rotation: 'rotation',
+//     scale: 'scale'
+// };
+
 Canvas.prototype.drawImage = function (data) {
     const { image, sx, sy, sw, sh, dx, dy, dw, dh } = data;
     this.context.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
 };
 
-Canvas.prototype.drawImageTree = function (root, branches = 'children') {
-    if (!root.hidden) {
+Canvas.prototype.drawImageTree = function (data) {
+    if (!data.hidden) {
         this.context.save();
-        this.context.translate(root.position.x, root.position.y);
-        this.context.rotate(root.rotation);
-        this.context.scale(root.scale.x, root.scale.y);
+        this.context.translate(data.position.x, data.position.y);
+        this.context.rotate(data.rotation);
+        this.context.scale(data.scale.x, data.scale.y);
 
-        if (root.image) {
-            this.drawImage(root);
+        if (data.image) {
+            this.drawImage(data);
         }
 
-        if (branches in root) {
-            for (const branch of root[branches]) {
-                this.drawImageTree(branch, branches);
+        if (data.children?.length) {
+            for (const child of data.children) {
+                this.drawImageTree(child);
             }
         }
 
