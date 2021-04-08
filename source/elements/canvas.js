b739e20f5ec25579d3ddfd1f122f50d9d6acb767
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-04-08 07:43:13 -0400

    make resize public

--- source/elements/canvas.js
+++ source/elements/canvas.js
@@ -11,7 +11,7 @@ export class Canvas extends Quantum {
         super();
 
         this.resize();
-        this.addEventListener('resize', this.#resize.bind(this));
+        this.addEventListener('resize', this.resize.bind(this));
     }
 
     connectedCallback() {
