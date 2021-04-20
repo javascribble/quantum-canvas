b3bd46311b2b31b3473dfd49d916b93ac42b5c22
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-04-20 11:39:34 -0400

    add load test

--- source/extensions/sprite.js
+++ source/extensions/sprite.js
@@ -1,8 +1,16 @@
 import { Canvas } from '../elements/canvas.js';
 
 Canvas.prototype.Sprite = class Sprite {
-    constructor(data) {
-        Object.assign(this, data);
+    constructor(image, sx = 0, sy = 0, sw = image.width, sh = image.height) {
+        this.image = image;
+        this.sx = sx;
+        this.sy = sy;
+        this.sw = sw;
+        this.sh = sh;
+        this.dx = -sw / 2;
+        this.dy = -sh / 2;
+        this.dw = sw;
+        this.dh = sh;
     }
 
     draw(context) {
--- test/debug.js
+++ test/debug.js
@@ -3,68 +3,43 @@ import '/source/extensions/node.js';
 import '/source/extensions/sprite.js';
 import '/source/main.js';
 
+const display = document.querySelector('#display');
 const canvas = document.querySelector('quantum-canvas');
-const [characterImage, tilemapImage] = document.querySelectorAll('img');
+const image = document.querySelector('img');
 
 const { Node, Sprite } = canvas;
 
-const mapSize = 10;
-const tileSize = 64;
+let count = 0;
+const root = new Node();
+const sprite = new Sprite(image);
+const animation = quantum.animate(({ delta, elapsed }) => {
+    const fps = Math.trunc(1000 / delta);
 
-const tiles = [];
-for (let row = 0; row < tilemapImage.height / tileSize; row++) {
-    for (let column = 0; column < tilemapImage.width / tileSize; column++) {
-        tiles.push({
-            image: tilemapImage,
-            sx: column * tileSize,
-            sy: row * tileSize,
-            sw: tileSize,
-            sh: tileSize
-        });
+    canvas.context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    for (let i = 0; i < 10; i++) {
+        const node = new Node();
+        node.drawables.push(sprite);
+        root.children.push(node);
+        count++;
     }
-}
 
-const tilemap = [];
-for (let i = 0; i < mapSize; i++) {
-    for (let ii = 0; ii < mapSize; ii++) {
-        tilemap.push(new Sprite({
-            ...tiles[Math.round(Math.random())],
-            dx: tileSize * (i % mapSize),
-            dy: tileSize * (ii % mapSize),
-            dw: tileSize,
-            dh: tileSize
-        }));
+    for (const { transform } of root.children) {
+        const { translation, rotation, scale } = transform;
+        rotation.z = Math.random() * Math.PI;
+        translation.x = (translation.x + Math.random() * 10) % canvas.clientWidth;
+        translation.y = (translation.y + Math.random() * 10) % canvas.clientHeight;
     }
-}
 
-const characterSprite = new Sprite({
-    image: characterImage,
-    sx: 0,
-    sy: 0,
-    sw: characterImage.width,
-    sh: characterImage.height,
-    dx: -characterImage.width / 2,
-    dy: -characterImage.height / 2,
-    dw: characterImage.width,
-    dh: characterImage.height
+    root.draw(canvas.context);
+
+    display.innerHTML = `FPS: ${fps} Count: ${count}`;
+
+    if (fps < 30) {
+        animation.stop();
+    }
 });
 
-const characterNode = new Node();
-characterNode.drawables.push(characterSprite);
-characterNode.transform.translation = { x: 400, y: 400 };
-
-const worldNode = new Node();
-worldNode.children.push(characterNode);
-worldNode.drawables = tilemap;
-
-quantum.animate(({ elapsed }) => {
-    const angle = elapsed * 0.005 % 360;
-    const radians = angle * Math.PI / 180;
-
-    characterNode.transform.rotation.z = radians;
-    worldNode.transform.translation.x = Math.sin(angle) * 10;
-
-    worldNode.draw(canvas.context);
-}).start();
+animation.start();
 
 document.body.style.visibility = 'visible';
