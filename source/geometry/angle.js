6c617987090e5c120f3c1db4d23bc246187bae54
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-06-11 15:11:17 -0400

    remove math

--- source/geometry/angle.js
+++
@@ -1,7 +0,0 @@
-export const convertRadians = radians => radians * 180 / Math.PI;
-
-export const convertDegrees = degrees => degrees * Math.PI / 180;
-
-export const clampRadians = radians => radians % (2 * Math.PI);
-
-export const clampDegrees = degrees => degrees % 360;
